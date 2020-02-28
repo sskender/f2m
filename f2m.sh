e64f6b3a7e67194de17b66a69fcd5cd8723d678a
sskender <sskender@users.noreply.github.com>
2020-02-28 21:11:25 +0100

    support to continue after script exit

--- f2m.sh
+++ f2m.sh
@@ -3,7 +3,7 @@
 F2MHOME="https://f2m.nyqcd.org"
 SAVE="F2M"
 
-mkdir $SAVE && cd $SAVE
+mkdir -p $SAVE && cd $SAVE
 
 # list of predmeti
 for i in \
@@ -11,13 +11,13 @@ for i in \
     grep "indexcolicon" | grep "\[DIR\]" | awk -F'href="' '{print $2}' | awk -F'">' '{print $1}')
 do
     # predmet folder and download
-    mkdir $i
+    mkdir -p $i
 
     for file in \
         $(curl $F2MHOME/$i | \
         grep "indexcolicon" | grep "td" | grep -v "PARENT" | awk -F'href="' '{print $2}' | awk -F'"><' '{print $1}')
     do
-        wget "$F2MHOME/$i/$file" -P $i
+        wget "$F2MHOME/$i/$file" -P $i --no-clobber --continue
     done
 
 done
